2ba5a9a7b7044d91543240312b103470c2279b72
Stefan Knutas <stefan.knutas@amf.se>
2026-01-28 14:57:56 +0100

    Activate AOT and change to compatible marshalling

--- Desktop.cs
+++ Desktop.cs
@@ -1,23 +1,52 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
 ﻿namespace SwitchDesktopAndMoveForegroundWindow;
 
 /// <summary>
 /// Provides access to Windows Virtual Desktop management functionality.
 /// </summary>
-public static class Desktop
+public static partial class Desktop
 {
     private const int LeftDirection = 3;
     private const int RightDirection = 4;
 
     private static readonly IVirtualDesktopManagerInternal VirtualDesktopManagerInternal;
 
+    [LibraryImport("ole32.dll")]
+    private static partial int CoCreateInstance(
+        ref Guid rclsid,
+        nint pUnkOuter,
+        uint dwClsContext,
+        ref Guid riid,
+        out nint ppv);
+
     static Desktop()
     {
+        const uint CLSCTX_LOCAL_SERVER = 0x4;
         var CLSID_ImmersiveShell = new Guid("C2F03A33-21F5-47FA-B4BB-156362A2F239");
+        var IID_IServiceProvider = typeof(IServiceProvider).GUID;
         var CLSID_VirtualDesktopManagerInternal = new Guid("C5E0CDCA-7B6E-41B2-9FC4-D93975CC467B");
         var CLSID_IVirtualDesktopManagerInternal = typeof(IVirtualDesktopManagerInternal).GUID;
 
-        var shell = (IServiceProvider)Activator.CreateInstance(Type.GetTypeFromCLSID(CLSID_ImmersiveShell)!)!;
-        VirtualDesktopManagerInternal = shell.QueryService(ref CLSID_VirtualDesktopManagerInternal, ref CLSID_IVirtualDesktopManagerInternal);
+        Marshal.ThrowExceptionForHR(CoCreateInstance(ref CLSID_ImmersiveShell, 0, CLSCTX_LOCAL_SERVER, ref IID_IServiceProvider, out var serviceProviderPointer));
+
+        try
+        {
+            IServiceProvider serviceProvider;
+            unsafe
+            {
+                serviceProvider = ComInterfaceMarshaller<IServiceProvider>.ConvertToManaged((void*)serviceProviderPointer)!;
+            }
+            VirtualDesktopManagerInternal = serviceProvider.QueryService(ref CLSID_VirtualDesktopManagerInternal, ref CLSID_IVirtualDesktopManagerInternal);
+        }
+        finally
+        {
+            if (serviceProviderPointer != 0)
+            {
+                Marshal.Release(serviceProviderPointer);
+            }
+        }
     }
 
     /// <summary>
@@ -32,12 +61,15 @@ public static class Desktop
     {
         var current = VirtualDesktopManagerInternal.GetCurrentDesktop();
         var dir = direction == Direction.Left ? LeftDirection : RightDirection;
-        var targetDesktop = VirtualDesktopManagerInternal.GetAdjacentDesktop(current, dir);
 
-        // If desktop is null it is probable an invalid target (e.g. no desktop to the left/right)
-        if (targetDesktop != null)
+        try
         {
+            var targetDesktop = VirtualDesktopManagerInternal.GetAdjacentDesktop(current, dir);
             VirtualDesktopManagerInternal.SwitchDesktopAndMoveForegroundView(targetDesktop);
+        }
+        catch (COMException)
+        {
+            // Probably invalid target desktop (e.g. no desktop to the left/right)
         }
     }
 
